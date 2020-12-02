fb1f7ca49e0bb38c44e66cc4102371dd04352cf3
crash3791 <goldenast@outlook.com>
2020-12-02 13:56:05 +0100

    Update admin, add news

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const createError = require("http-errors");
+const cookieSession = require("cookie-session");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const config = require("./config");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("DB conect");
+});
 
 const indexRouter = require("./routes/index");
 const newsRouter = require("./routes/news");
@@ -20,6 +34,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: config.keySession,
+
+    // Cookie Options
+    maxAge: config.maxAgeSession,
+  })
+);
 
 app.use(function (req, res, next) {
   res.locals.path = req.path;
